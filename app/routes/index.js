c6b7520998be864df7eab2087c227ac3ef4675af
katkatprog <katkatprog@gmail.com>
2025-04-29 16:37:48 +0900

    DB値表示

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,14 @@
+const { PrismaClient } = require('@prisma/client');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const prisma = new PrismaClient();
+  const memoList = await prisma.memo.findMany();
+  console.log(memoList);
+  prisma.$disconnect();
+  res.render('index', { title: 'Express', memoList });
 });
 
 module.exports = router;
